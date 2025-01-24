f8dc734355725fd26180a23a155ae6bb88669ff1
pallavi-a11y <papnaipallavi672@gmail.com>
2025-01-24 18:38:24 +0530

    empty commit

--- # git-clone-https-git.codecrafters.c
+++ # git-clone-https-git.codecrafters.c
@@ -1,4 +1,6 @@
-# git-clone-https-git.codecrafters.io-2f67d94bfd46803a-codecrafters-redis-c-cd-codecrafters-redis-c
-cd /path/to/your/repository
+#git-clone-https-git.codecrafters.io-2f67d94bfd46803a-codecrafters-redis-c-cd-codecrafters-redis-c
+cd/path/to/your/repository
 git commit --allow-empty -m 'test'
 git push origin master
+code --goto app/server.c:line:column
+code app/server.c
